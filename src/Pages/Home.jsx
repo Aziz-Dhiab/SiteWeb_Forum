ac6bbde5fa2c9e1df192c31387c0537100938195
bahmed99 <user.email>
2021-10-05 15:17:15 +0100

    d

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import react from 'react'
 import Navbar from '../Components/Navbar'
 import '../Assets/css/homepage.css'
+import Partenaires from '../Components/partenaires/Partenaires'
+import Footer from '../Components/Footer'
 export default function Home() {
     return(
-       <div className='homepage'>
+       <div className=''>
        <Navbar/>
+       <Partenaires />
+       <Footer />
        </div>
     )
 }
