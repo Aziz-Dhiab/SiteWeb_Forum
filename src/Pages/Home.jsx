6b4d498e1751ebbfbda2f38a457185c436027ac9
Your NameAziz-Dhiab <aziz_dhiab@hotmail.com>
2021-10-06 17:03:05 +0100

    speakers

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,11 @@
 import react from 'react'
 import Navbar from '../Components/Navbar'
+import Speakers from '../Components/Speakers'
 import '../Assets/css/homepage.css'
 export default function Home() {
     return(
        <div className='homepage'>
-       <Navbar/>
+       <Speakers/>
        </div>
     )
 }
