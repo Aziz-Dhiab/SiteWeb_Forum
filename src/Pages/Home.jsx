a8034fb71822ff31f9ff413c3cc94ddefb06733e
Bochra Mzid <bochra.mzid@ensi-uma.tn>
2021-10-07 19:39:52 +0100

    home header

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
-import react from 'react'
+import React from 'react'
 import Navbar from '../Components/Navbar'
 import '../Assets/css/homepage.css'
+import Header from "../Components/HomeHeader"
 export default function Home() {
     return(
        <div className='homepage'>
        <Navbar/>
+       <Header/>
        </div>
     )
 }
