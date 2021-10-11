d6434459bfc85b58b49b11f1593f61ff599a9730
Your NameAziz-Dhiab <aziz_dhiab@hotmail.com>
2021-10-11 10:39:54 +0100

    added responsive

--- src/Components/Speakers.jsx
+++ src/Components/Speakers.jsx
@@ -4,14 +4,16 @@ import image from '../Assets/images/img.jfif'
 
 function Speakers() {
     return (
+    
     <div className = "speakers_body">
         <div className = "slider_speakers">
-            <span style = {{"--i":4}}><div><img src = {image} alt = ""></img>Speaker 1</div></span>
-            <span style = {{"--i":3}}><div><img src = {image} alt = ""></img>Speaker 2</div></span>
-            <span style = {{"--i":2}}><div><img src = {image} alt = ""></img>Speaker 3</div></span>
-            <span style = {{"--i":1}}><div><img src = {image} alt = ""></img>Speaker 4</div></span>
+            <span style = {{"--i":4}}><div><img src = {image} alt = ""></img><br/>Speaker 1</div></span>
+            <span style = {{"--i":3}}><div><img src = {image} alt = ""></img><br/>Speaker 2</div></span>
+            <span style = {{"--i":2}}><div><img src = {image} alt = ""></img><br/>Speaker 3</div></span>
+            <span style = {{"--i":1}}><div><img src = {image} alt = ""></img><br/>Speaker 4</div></span>
         </div>
     </div>
+    
     )
 }
 
