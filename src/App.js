536951e6906a35d4c9461f7f94ac3f7a8c118f46
Bochra Mzid <bochra.mzid@ensi-uma.tn>
2021-10-16 00:23:23 +0100

    articles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './Assets/css/app.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home'
 import Auditorium from './Components/Auditorium';
+import Articles from "./Pages/Articles"
+import Article from "./Pages/ArticlePage"
 import Navbar from './Components/Navbar';
 import { Nav } from 'react-bootstrap';
 function App() {
@@ -14,6 +16,9 @@ function App() {
         <Switch>
           <Route exact path="/"><Home/></Route>
           <Route exact path="/programme"><Auditorium/></Route>
+          <Route exact path="/articles"><Articles/></Route>
+          <Route exact path="/articles/:articleId"><Article/></Route>
+
        
         </Switch>
       </Router> 
